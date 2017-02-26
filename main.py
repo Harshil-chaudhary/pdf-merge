34c285aa5a4036a06f6d9e9b59f58af0fa265dea
Harshil-chaudhary <harshilchaudhary8@gmail.com>
2017-02-26 16:41:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from pyPdf import PdfFileWriter, PdfFileReader
+
+def merge_pdf(input,output):
+    [output.addPage(input.getPage(page_num)) for page_num in range(input.numPages)]
+
+output = PdfFileWriter()
+
+# Appending  pdf-pages from ten different files in order
+merge_pdf(PdfFileReader(open("1.pdf","rb")),output)
+merge_pdf(PdfFileReader(open("2.pdf","rb")),output)
+merge_pdf(PdfFileReader(open("3.pdf","rb")),output)
+merge_pdf(PdfFileReader(open("4.pdf","rb")),output)
+merge_pdf(PdfFileReader(open("5.pdf","rb")),output)
+merge_pdf(PdfFileReader(open("6.pdf","rb")),output)
+merge_pdf(PdfFileReader(open("7.pdf","rb")),output)
+merge_pdf(PdfFileReader(open("8.pdf","rb")),output)
+merge_pdf(PdfFileReader(open("9.pdf","rb")),output)
+merge_pdf(PdfFileReader(open("10.pdf","rb")),output)
+
+
+# Writing all the collected pages to a file
+output.write(open("output.pdf","wb"))
